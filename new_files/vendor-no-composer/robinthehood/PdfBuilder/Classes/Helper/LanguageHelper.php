293dc61c40e0714cd3e46daeaba98bbd0bf48c74
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-23 18:12:40 +0100

    chore: add LanguageHelper

---
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Helper/LanguageHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace RobinTheHood\PdfBuilder\Classes\Helper;
+
+class LanguageHelper
+{
+    public static function loadLanguageFile($language, $file)
+    {
+        // Versuche zuerst die Sprachdatei aus dem Template zu laden.
+        $templateLangPath = DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/lang/' . $language . '/' . $file;
+        if (\file_exists($templateLangPath)) {
+            require_once $templateLangPath;
+            return;
+        }
+
+        // Exsistiert die Sprachdatei im Template nicht, lade die Datei aus dem Lang-Ordner.
+        $langPath = DIR_FS_CATALOG . 'lang/' . $language . '/' . $file;
+        if (\file_exists($langPath)) {
+            require_once $langPath;
+        }
+    }
+}
