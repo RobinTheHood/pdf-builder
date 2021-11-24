485efe1eaa2ff97c2c099e2c3d5365fdaa2a65f3
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-24 21:52:49 +0100

    chore: add new test

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Tests/PdfBuilderTest.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Tests/PdfBuilderTest.php
@@ -5,11 +5,22 @@ declare(strict_types=1);
 namespace RobinTheHood\PdfBuilder\Classes\Tests;
 
 use RobinTheHood\PdfBuilder\Classes\PdfBill;
+use RobinTheHood\PdfBuilder\Classes\Templates\Letter;
 
 class PdfBuilderTest
 {
     public function test1(): void
     {
         $pdfBill = new PdfBill();
+        $pdfBill->addBill(27);
+        $pdf = $pdfBill->getPdf();
+
+        $pdf->Output();
+    }
+
+    public function test2(): void
+    {
+        $letter = new Letter();
+        $letter->render();
     }
 }
