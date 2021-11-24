b5a9bc9586b7cb99180a9efa8f660ed6404cc60f
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-24 21:51:30 +0100

    chore: add new test

--- new_files/rth_pdf_builder_test.php
+++ new_files/rth_pdf_builder_test.php
@@ -3,13 +3,16 @@
 use RobinTheHood\PdfBuilder\Classes\Tests\PdfBuilderTest;
 
 require_once 'includes/application_top.php';
+require_once DIR_WS_CLASSES . 'order.php';
+require_once DIR_FS_INC . 'xtc_format_price_order.inc.php';
+require_once DIR_FS_INC . 'xtc_get_attributes_model.inc.php';
 require_once DIR_FS_DOCUMENT_ROOT . '/vendor-no-composer/autoload.php';
 
 restore_error_handler();
 restore_exception_handler();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL ^ E_NOTICE);
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
 
 $pdfBuilderTest = new PdfBuilderTest();
-$pdfBuilderTest->test1();
+$pdfBuilderTest->test2();
