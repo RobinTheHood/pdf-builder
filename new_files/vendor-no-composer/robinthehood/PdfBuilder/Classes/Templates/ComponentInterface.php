221661fbf5ec119a8fe4feccab7c0f3588e3a515
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-24 21:49:08 +0100

    chore: add ComponentInterface

---
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Templates/ComponentInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RobinTheHood\PdfBuilder\Classes\Templates;
+
+use RobinTheHood\PdfBuilder\Classes\Pdf\Pdf;
+
+interface ComponentInterface
+{
+    public function render(Pdf $pdf);
+}
