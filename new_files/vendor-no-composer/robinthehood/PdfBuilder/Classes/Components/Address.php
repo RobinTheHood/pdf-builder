6218e1afafb3fcc11ff17c860b2ad01b0cd13fbf
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-25 22:33:24 +0100

    chore: add sender address

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Components/Address.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Components/Address.php
@@ -13,20 +13,33 @@ class Address implements ComponentInterface
     private $basePositionX = 20; // Unit: mm
     private $basePositionY = 27; // Unit: mm
 
-    private $addressZone = null;
+    private $address = null;
+    private $sender = null;
 
     public function __construct()
     {
-        $this->addressZone = new TextArea();
-        $this->addressZone->setPosition($this->basePositionX + 4, $this->basePositionY + 17.7);
-        $this->addressZone->setDimention(85 - 4, 27.3);
-        $this->addressZone->setFontSize(10);
-        $this->addressZone->setLineHeight(4.55);
+        $this->address = new TextArea();
+        $this->address->setPosition($this->basePositionX + 4, $this->basePositionY + 17.7);
+        $this->address->setDimention(85 - 4, 27.3);
+        $this->address->setFontSize(10);
+        $this->address->setLineHeight(4.55);
+
+        $this->sender = new TextArea();
+        $this->sender->setPosition($this->basePositionX + 4, $this->basePositionY);
+        $this->sender->setDimention(85 - 4, 17.7);
+        $this->sender->setFontSize(7);
+        $this->sender->setLineHeight(3.5);
+        $this->sender->setVerticalAlign(TextArea::VERTICAL_ALIGN_BOTTOM);
     }
 
     public function setAddress(string $address): void
     {
-        $this->addressZone->setText($address);
+        $this->address->setText($address);
+    }
+
+    public function setSender(string $sender): void
+    {
+        $this->sender->setText($sender);
     }
 
     public function render(Pdf $pdf): void
@@ -35,15 +48,16 @@ class Address implements ComponentInterface
 
         // Hole Component Area
         $pdf->SetXY($this->basePositionX, $this->basePositionY); // Unit: mm
-        $pdf->Cell(85, 45, '', 1);
+        //$pdf->Cell(85, 45, '', 1);
 
         // Sender Zone
         $pdf->SetXY($this->basePositionX, $this->basePositionY); // Unit: mm
-        $pdf->Cell(85, 17.7, '', 1);
+        //$pdf->Cell(85, 17.7, '', 1);
+        $this->sender->render($pdf);
 
         // Address Zone
         $pdf->SetXY($this->basePositionX, $this->basePositionY + 17.7); // Unit: mm
-        $pdf->Cell(85, 27.3, '', 1);
-        $this->addressZone->render($pdf);
+        //$pdf->Cell(85, 27.3, '', 1);
+        $this->address->render($pdf);
     }
 }
