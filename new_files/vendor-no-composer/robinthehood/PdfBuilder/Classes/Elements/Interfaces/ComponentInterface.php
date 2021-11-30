fe48bbf3cfb64bcb26bdc83d90bffd937ab4a37b
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-30 17:54:09 +0100

    chore: add constant POSITION_MODE

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Elements/Interfaces/ComponentInterface.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Elements/Interfaces/ComponentInterface.php
@@ -8,5 +8,8 @@ use RobinTheHood\PdfBuilder\Classes\Pdf\Pdf;
 
 interface ComponentInterface
 {
+    public const POSITION_MODE_RELATIVE = 0;
+    public const POSITION_MODE_ABSOLUTE = 1;
+
     public function render(Pdf $pdf);
 }
