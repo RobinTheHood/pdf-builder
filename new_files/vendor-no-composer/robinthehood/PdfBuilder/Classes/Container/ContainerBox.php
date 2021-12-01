021ae91bc57d01b6d843c3143625c2779b9a8b66
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-01 19:38:39 +0100

    chore: add new methods to ContainerBox

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/ContainerBox.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/ContainerBox.php
@@ -79,4 +79,174 @@ class ContainerBox
 
         return $containerBox;
     }
+
+    public function getMarginBox(): array
+    {
+        $x = $this->positionX->getValue();
+        $y = $this->positionY->getValue();
+
+        $width = $this->marginLeft->getValue()
+            + $this->borderLeft->getValue()
+            + $this->paddingLeft->getValue()
+            + $this->width->getValue()
+            + $this->paddingRight->getValue()
+            + $this->borderRight->getValue()
+            + $this->marginRight->getValue();
+
+        $height = $this->marginTop->getValue()
+            + $this->borderTop->getValue()
+            + $this->paddingTop->getValue()
+            + $this->height->getValue()
+            + $this->paddingBottom->getValue()
+            + $this->borderBottom->getValue()
+            + $this->marginBottom->getValue();
+
+        $boxLines = $this->getBoxLines($x, $y, $width, $height);
+
+        return [
+            'x' => $x,
+            'y' => $y,
+            'width' => $width,
+            'height' => $height,
+            'boxLines' => $boxLines,
+        ];
+    }
+
+    public function getBorderBox(): array
+    {
+        $x = $this->positionX->getValue()
+            + $this->marginLeft->getValue();
+
+        $y = $this->positionY->getValue()
+            + $this->marginTop->getValue();
+
+        $width = $this->borderLeft->getValue()
+            + $this->paddingLeft->getValue()
+            + $this->width->getValue()
+            + $this->paddingRight->getValue()
+            + $this->borderRight->getValue();
+
+        $height = $this->borderTop->getValue()
+            + $this->paddingTop->getValue()
+            + $this->height->getValue()
+            + $this->paddingBottom->getValue()
+            + $this->borderBottom->getValue();
+
+        $boxLines = $this->getBoxLines($x, $y, $width, $height);
+
+        return [
+            'x' => $x,
+            'y' => $y,
+            'width' => $width,
+            'height' => $height,
+            'boxLines' => $boxLines,
+        ];
+    }
+
+    public function getPaddingBox(): array
+    {
+        $x = $this->positionX->getValue()
+            + $this->marginLeft->getValue()
+            + $this->borderLeft->getValue();
+
+        $y = $this->positionY->getValue()
+            + $this->marginTop->getValue()
+            + $this->borderTop->getValue();
+
+        $width = $this->paddingLeft->getValue()
+            + $this->width->getValue()
+            + $this->paddingRight->getValue();
+
+        $height = $this->paddingTop->getValue()
+            + $this->height->getValue()
+            + $this->paddingBottom->getValue();
+
+
+        $boxLines = $this->getBoxLines($x, $y, $width, $height);
+
+        return [
+            'x' => $x,
+            'y' => $y,
+            'width' => $width,
+            'height' => $height,
+            'boxLines' => $boxLines,
+        ];
+    }
+
+    public function getContentBox(): array
+    {
+        $x = $this->positionX->getValue()
+            + $this->marginLeft->getValue()
+            + $this->borderLeft->getValue()
+            + $this->paddingLeft->getValue();
+
+        $y = $this->positionY->getValue()
+            + $this->marginTop->getValue()
+            + $this->borderTop->getValue()
+            + $this->paddingTop->getValue();
+
+        $width = $this->width->getValue();
+        $height = $this->height->getValue();
+
+        $boxLines = $this->getBoxLines($x, $y, $width, $height);
+
+        return [
+            'x' => $x,
+            'y' => $y,
+            'width' => $width,
+            'height' => $height,
+            'boxLines' => $boxLines,
+        ];
+    }
+
+    private function getBoxLines($x, $y, $width, $height)
+    {
+        $top = [
+            'x1' => $x,
+            'y1' => $y,
+            'x2' => $x + $width,
+            'y2' => $y
+        ];
+
+        $right = [
+            'x1' => $x + $width,
+            'y1' => $y,
+            'x2' => $x + $width,
+            'y2' => $y + $height
+        ];
+
+        $bottom = [
+            'x1' => $x,
+            'y1' => $y + $height,
+            'x2' => $x + $width,
+            'y2' => $y + $height,
+        ];
+
+        $left = [
+            'x1' => $x,
+            'y1' => $y,
+            'x2' => $x,
+            'y2' => $y + $height
+        ];
+
+        return [
+            'top' => $top,
+            'right' => $right,
+            'bottom' => $bottom,
+            'left' => $left
+        ];
+    }
+
+    public function getMarginBottom($containers)
+    {
+        $bottomLine = 0;
+        foreach ($containers as $container) {
+            $box = $container->getCalcedContainer()->getMarginBox();
+            $containBottomLine = $box['y'] + $box['height'];
+            if ($containBottomLine > $bottomLine) {
+                $bottomLine = $containBottomLine;
+            }
+        }
+        return $bottomLine;
+    }
 }
