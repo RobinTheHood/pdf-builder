97e0e13235122445477ca6f3596f3c5047420cd2
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-01 19:10:41 +0100

    chore: add class ContainerPdfRenderer

---
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/ContainerPdfRenderer.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RobinTheHood\PdfBuilder\Classes\Container;
+
+use RobinTheHood\PdfBuilder\Classes\Container\Interfaces\ContainerInterface;
+use RobinTheHood\PdfBuilder\Classes\Container\Interfaces\ContainerRendererCanvasInterface;
+use RobinTheHood\PdfBuilder\Classes\Container\Interfaces\ContainerRendererInterface;
+
+class ContainerPdfRenderer implements ContainerRendererInterface
+{
+    public function render(ContainerRendererCanvasInterface $canvas, ContainerInterface $container): void
+    {
+        $renderContainer = $container->getCalcedContainer();
+
+        // Draw Margin Box
+        $box = $renderContainer->getMarginBox();
+        $color = $this->createColor(255, 0, 0);
+        $this->drawBox($canvas, $box, $color);
+
+        if (true) {
+            // Draw Border Box
+            $box = $renderContainer->getBorderBox();
+            $color = $this->createColor(0, 255, 255);
+            $this->drawBox($canvas, $box, $color);
+
+            // Draw Padding Box
+            $box = $renderContainer->getPaddingBox();
+            $color = $this->createColor(0, 0, 255);
+            $this->drawBox($canvas, $box, $color);
+
+            // Draw Content Box
+            $box = $renderContainer->getContentBox();
+            $colorBlue = $this->createColor(0, 255, 255);
+            $this->drawBox($canvas, $box, $colorBlue);
+        }
+
+        $this->renderChilds($canvas, $container);
+    }
+
+    private function renderChilds(ContainerRendererCanvasInterface $canvas, ContainerInterface $container)
+    {
+        foreach ($container->getChildContainers() as $childContainer) {
+            $containerRenderer = $childContainer->getContainerRenderer();
+            $containerRenderer->render($canvas, $childContainer);
+        }
+    }
+
+    private function drawBox(ContainerRendererCanvasInterface $canvas, array $box, $color)
+    {
+        $canvas->setColor($color['r'], $color['g'], $color['b']);
+
+        // Top
+        $line = $box['boxLines']['top'];
+        $canvas->drawLine($line['x1'], $line['y1'], $line['x2'], $line['y2']);
+
+        // Right
+        $line = $box['boxLines']['right'];
+        $canvas->drawLine($line['x1'], $line['y1'], $line['x2'], $line['y2']);
+
+        // Bottom
+        $line = $box['boxLines']['bottom'];
+        $canvas->drawLine($line['x1'], $line['y1'], $line['x2'], $line['y2']);
+
+        // Left
+        $line = $box['boxLines']['left'];
+        $canvas->drawLine($line['x1'], $line['y1'], $line['x2'], $line['y2']);
+    }
+
+    private function createColor($r, $g, $b)
+    {
+        return [
+            'r' => $r,
+            'g' => $g,
+            'b' => $b
+        ];
+    }
+}
