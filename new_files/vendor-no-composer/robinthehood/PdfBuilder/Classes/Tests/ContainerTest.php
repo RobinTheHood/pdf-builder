39c49209f30560c08e1bdb4e409c4b3df6ea4226
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-01 23:26:23 +0100

    chore: change test

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Tests/ContainerTest.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Tests/ContainerTest.php
@@ -39,63 +39,44 @@ class ContainerTest
     public function test1(): void
     {
         $baseContainer = new Container();
-        // $baseContainer->setPosition(10, 10);
         $baseContainer->containerBox->positionX->setValue(10);
         $baseContainer->containerBox->positionY->setValue(10);
-        // $baseContainer->setPadding(20, 20);
-        // $baseContainer->setBorder(10, 10);
-        // $baseContainer->setDimentions(self::DEFAULT_WIDTH - 20, self:: DEFAULT_HEIGHT - 20);
         $baseContainer->containerBox->width->setValue(self::DEFAULT_WIDTH - 20);
         $baseContainer->containerBox->height->setValue(self:: DEFAULT_HEIGHT - 20);
 
         $container1 = new Container();
-        //$container1->setPosition(20, 20);
-        //$container1->setDimentions(180, 20);
         $container1->containerBox->width->setValue(180);
         $container1->containerBox->height->setValue(20);
+        $container11 = new Container();
+        $container11->containerBox->width->setValue(80);
+        $container11->containerBox->height->setValue(20);
+        $container12 = new Container();
+        $container12->containerBox->width->setValue(60);
+        $container12->containerBox->height->setValue(20);
+        $container12->containerBox->marginLeft->setValue(10);
+        $container1->addChildContainer($container11);
+        $container1->addChildContainer($container12);
 
-        //$container1->setMargin(10, 10);
-        //$container1->setBorder(2, 2, 2, 2);
-        //$container1->setPadding(10, 20, 30, 40);
-
-        $container2 = $container1->copy();
-        // $container2->setDimentions(100, 20);
+        $container2 = new Container();
         $container2->containerBox->width->setValue(100);
         $container2->containerBox->height->setValue(20);
 
-        $container3 = $container1->copy();
-        // $container3->setDimentions(0, 20);
+        $container3 = new Container();
+        $container3->containerBox->width->setValue(140);
         $container3->containerBox->height->setValue(20);
+        $container31 = new Container();
+        $container31->containerBox->width->setValue(120);
+        $container31->containerBox->height->setValue(20);
+        $container32 = new Container();
+        $container32->containerBox->width->setValue(130);
+        $container32->containerBox->height->setValue(20);
+        $container3->addChildContainer($container31);
+        $container3->addChildContainer($container32);
 
-        $container4 = $container1->copy();
-        // $container4->setDimentions(70, 20);
-        $container4->containerBox->width->setValue(70);
-        $container4->containerBox->height->setValue(20);
-
-        $container5 = $container1->copy();
-        // $container5->setDimentions(60, 20);
-        $container5->containerBox->width->setValue(60);
-        $container5->containerBox->height->setValue(20);
-        // $container4->setMargin(0, 0, 0, 10);
-        $container5->containerBox->marginLeft->setValue(10);
-
-        $container1->addChildContainer($container4);
-        $container1->addChildContainer($container5);
-
-        $container6 = $container1->copy();
-        // $container6->setDimentions(10, 20);
-        $container6->containerBox->width->setValue(10);
-        $container6->containerBox->height->setValue(20);
-
-        // $container7 = $container1->copy();
-        // $container7->setDimentions(20, 20);
 
         $baseContainer->addChildContainer($container1);
         $baseContainer->addChildContainer($container2);
-        $baseContainer->addChildContainer($container6);
-        // $baseContainer->addChildContainer($container3);
-        // $baseContainer->addChildContainer($container4);
-        // $baseContainer->addChildContainer($container5);
+        $baseContainer->addChildContainer($container3);
 
         $baseContainer->calcAll();
 
