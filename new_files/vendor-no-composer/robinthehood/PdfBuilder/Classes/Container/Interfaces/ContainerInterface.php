6689e078083540247d7a174b2609a06840afb920
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-01 19:41:05 +0100

    refactor: ContainerInterface

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/Interfaces/ContainerInterface.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/Interfaces/ContainerInterface.php
@@ -6,20 +6,6 @@ namespace RobinTheHood\PdfBuilder\Classes\Container\Interfaces;
 
 interface ContainerInterface
 {
-    public function getMarginBox(): array;
-
-    public function getBorderBox(): array;
-
-    public function getPaddingBox(): array;
-
-    public function getContentBox(): array;
-
-    public function setMargin($top, $right = null, $bottom = null, $left = null): void;
-
-    public function setBorder($top, $right = null, $bottom = null, $left = null): void;
-
-    public function setPadding($top, $right = null, $bottom = null, $left = null): void;
-
     public function getChildContainers(): array;
 
     public function getContainerRenderer(): ContainerRendererInterface;
