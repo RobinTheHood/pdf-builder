2b10173799d148fa1fe02a865edc1a43bdf709d0
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-01 19:03:21 +0100

    chore: add class ContainerValue

---
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/ContainerValue.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RobinTheHood\PdfBuilder\Classes\Container;
+
+class ContainerValue
+{
+    public const UNIT_UNCHANGED = 0;
+    public const UNIT_PIXEL = 1;
+    public const UNIT_PERCENT = 2;
+    public const UNIT_AUTO = 3;
+
+    private $value;
+    private $isSet;
+    private $unit;
+
+    public function getValue(): float
+    {
+        return $this->value;
+    }
+
+    public function getUnit(): float
+    {
+        return $this->unit;
+    }
+
+    public function isSet(): bool
+    {
+        return $this->isSet;
+    }
+
+    public function set(float $value, $unit = self::UNIT_PIXEL): void
+    {
+        $this->setValue($value);
+        $this->setUnit($unit);
+    }
+
+    public function setValue(float $value): void
+    {
+        $this->value = $value;
+        $this->isSet = true;
+    }
+
+    public function setUnit(int $unit): void
+    {
+        $this->unit = $unit;
+    }
+
+    public function copy(): ContainerValue
+    {
+        $containerValue = new ContainerValue();
+        $containerValue->value = $this->value;
+        $containerValue->isSet = $this->isSet;
+        $containerValue->unit = $this->unit;
+        return $containerValue;
+    }
+}
