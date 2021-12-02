04d69444dfcb831ccb73388789eca79df9023632
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-02 11:10:16 +0100

    chore: add test for component ContentArea

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Templates/Bill.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Templates/Bill.php
@@ -14,6 +14,7 @@ use RobinTheHood\PdfBuilder\Classes\Elements\Table;
 use RobinTheHood\PdfBuilder\Classes\Elements\FooterDecorator;
 use RobinTheHood\PdfBuilder\Classes\Components\FoldMark;
 use RobinTheHood\PdfBuilder\Classes\Components\Address;
+use RobinTheHood\PdfBuilder\Classes\Components\ContentArea;
 use RobinTheHood\PdfBuilder\Classes\Components\Infoblock;
 use RobinTheHood\PdfBuilder\Classes\Components\OrderTable;
 use RobinTheHood\PdfBuilder\Classes\Components\OrderTotalTable;
@@ -23,6 +24,11 @@ class Bill
     private $document;
 
     public function __construct()
+    {
+        $this->createBill2();
+    }
+
+    private function createBill1(): void
     {
         $section = new Section();
 
@@ -88,6 +94,102 @@ class Bill
         $contentOutoText->setFontWeight(PDF::FONT_WEIGHT_NORMAL);
         $contentOutoText->setText("Vielen Dank für Ihren Auftrag. Besuchen Sie uns wieder unter online-shop.de. Leistungsdatum entspricht Rechnungsdatum. Es gelten unsere Allgemeinen Geschäftsbedingungen.");
         $section->addChildComponent($contentOutoText);
+
+        $dinImage = new Image(DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/img/din_5008_a.png');
+        //$dinImage = new Image(DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/img/rechnung_demo_1.png');
+        $dinImage->setPositionX(0);
+        $dinImage->setPositionY(0);
+        $dinImage->setWidth(210);
+
+        $pageDecorator = new PageDecorator();
+        //$pageDecorator->addChildComponent($dinImage);
+        $pageDecorator->addChildComponent(new FoldMark());
+
+        $tableFooter = new Table();
+
+        $width = 60;
+        $tableFooter->setColumnWidths([
+            $width,
+            $width,
+            $width,
+        ]);
+
+        $tableFooter->addRow([
+            ['content' => "MAX MUSTERFIRMA\nMustermann Straße 1\n12345 Musterstadt", 'alignment' => Pdf::CELL_ALIGN_LEFT],
+            ['content' => "www.musterfirma.de\ninfo@musterfirma.de\n+49 1234 1111-0", 'alignment' => Pdf::CELL_ALIGN_LEFT],
+            ['content' => "BLZ: 123456789\nIBAN: DE123456789123456789\nUSt-ID: DE123456789", 'alignment' => Pdf::CELL_ALIGN_LEFT],
+        ], ['fontWeight' => Pdf::FONT_WEIGHT_NORMAL, 'border' => Table::ROW_BORDER_NONE]);
+
+        $footer = new FooterDecorator();
+        $footer->addChildComponent($tableFooter);
+
+        $section->setPageDecorator($pageDecorator);
+        $section->setFooterDecorator($footer);
+
+        $this->document = new Document();
+        $this->document->addSection($section);
+        $this->document->addSection($section);
+    }
+
+    private function createBill2(): void
+    {
+        $section = new Section();
+
+        // Logo
+        $logo = new Image(DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/img/rth_logo.png');
+        $logo->setPositionX(145);
+        $logo->setPositionY(9);
+        $logo->setWidth(40);
+        $section->addChildComponent($logo);
+
+        // Address
+        $address = new Address();
+        $address->setAddress("Musterfirma GmbH\nz.H. Max Mustermann\nHauptstraße 999\n12345 Neustadt\nDeutschland");
+        $address->setSender("Max Mustermann - 12345 Neustadt 1\n");
+        $section->addChildComponent($address);
+
+        // Infoblock
+        $infoblock = new Infoblock();
+        $section->addChildComponent($infoblock);
+
+        // Content Area
+        $contentArea = new ContentArea();
+
+
+        // Heading
+        $contentHeading = new TextArea();
+        //$contentHeading->setBounds(25, 103.46, 175, 10);
+        //$contentHeading->setDimention(175, 10);
+        $contentHeading->setFontSize(18);
+        $contentHeading->setFontWeight(PDF::FONT_WEIGHT_BOLD);
+        $contentHeading->setText('Rechnung');
+        $contentArea->addChildComponent($contentHeading);
+
+        // Content Intro Text
+        $contentIntroText = new TextArea();
+        //$contentIntroText->setBounds(25, 103.46 + 10, 175, 10);
+        $contentIntroText->setDimention(175, 10);
+        $contentIntroText->setFontSize(10);
+        $contentIntroText->setLineHeight(5); // Unit: mm
+        $contentIntroText->setFontWeight(PDF::FONT_WEIGHT_NORMAL);
+        $contentIntroText->setText("Sehr geehrte Frau Lena Musterfrau,\nwir freuen uns, dass Sie bei online-shop.de bestellt haben.\nDiese Zeile ist zuviel.");
+        $contentArea->addChildComponent($contentIntroText);
+
+        $section->addChildComponent($contentArea);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         $dinImage = new Image(DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/img/din_5008_a.png');
         //$dinImage = new Image(DIR_FS_CATALOG . 'templates/' . CURRENT_TEMPLATE . '/img/rechnung_demo_1.png');
