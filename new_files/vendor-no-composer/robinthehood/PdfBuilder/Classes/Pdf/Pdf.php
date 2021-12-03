2cb052aafa03d1923490be5ed44c6fe0296ba0eb
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-03 21:42:59 +0100

    chore: add const POINTS_PER_MM to Pdf class

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Pdf/Pdf.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Pdf/Pdf.php
@@ -9,6 +9,8 @@ use RobinTheHood\Tfpdf\Tfpdf;
 
 class Pdf extends Tfpdf implements ContainerRendererCanvasInterface
 {
+    public const POINTS_PER_MM = 72 / 25.4;
+
     public const CELL_NEW_LINE_OFF = 0;
     public const CELL_NEW_LINE = 1;
     public const CELL_NEW_LINE_BELOW = 2;
