8d2f6293813f661266dbfc8592b21cf45719883a
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-04 18:03:49 +0100

    chore: do auto page break experiments

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Pdf/Pdf.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Pdf/Pdf.php
@@ -90,6 +90,67 @@ class Pdf extends Tfpdf implements ContainerRendererCanvasInterface
         $this->SetDrawColor((int) $r, (int) $g, (int) $b);
     }
 
+    public function drawText(string $text, float $x, float $y, float $width, float $height): void
+    {
+        // $this->breakPage($y, $height);
+        // $relativPageNo = $this->getRelativPageNo();
+        // $y = $this->mapYOnPage($y, $relativPageNo);
+
+        $newY = $this->mapYOnPage($y, $height);
+        $this->doPageBreak($newY);
+
+        $this->SetXY($x, $newY['yOnPage']);
+        $this->Cell($width, $height, $text, self::CELL_BORDER_NONE, self::CELL_NEW_LINE_BELOW);
+    }
+
+    private $yOffset = 0;
+    private $lastResetPageNo = 0;
+
+    private $firstPageMargin = [
+        'top' => 0,
+        'bottom' => 35
+    ];
+
+    private $pageMargin = [
+        'top' => 10,
+        'bottom' => 35
+    ];
+
+
+    public function breakPage($y, $height): bool
+    {
+        $relativPageNo = $this->PageNo() - $this->lastResetPageNo;
+        $pageMargin = $this->getPageMargin($relativPageNo);
+        $pageHeight = 297 - $pageMargin['bottom'] - $pageMargin['top'];
+        $page = ($y + $height) / $pageHeight;
+
+        if ($relativPageNo < $page) {
+            $this->addPage();
+            $this->SetXY(0, 0);
+            $relativPageNo = $this->PageNo() - $this->lastResetPageNo;
+            $pageMargin = $this->getPageMargin($relativPageNo);
+            $this->yOffset += $pageHeight - $pageMargin['top'];
+
+            $this->SetDrawColor(255, 0, 0);
+            $this->Line(0, $pageMargin['top'], 210, $pageMargin['top']);
+
+            $this->SetDrawColor(255, 0, 0);
+            $this->Line(0, 297 - $pageMargin['bottom'], 210, 297 - $pageMargin['bottom']);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private function getPageMargin(int $relativPageNo): array
+    {
+        if ($relativPageNo == 1) {
+            return $this->firstPageMargin;
+        }
+        return $this->pageMargin;
+    }
+
     private function cutLine(float $x1, float $y1, float $x2, float $y2, float $pageSize): array
     {
         $lines = [];
@@ -197,5 +258,72 @@ class Pdf extends Tfpdf implements ContainerRendererCanvasInterface
     {
         $this->lastOffset = 0;
         $this->lastPageNo = $this->PageNo();
+        $this->yOffset = 0;
+        $this->lastResetPageNo = $this->PageNo() - 1;
+    }
+
+
+
+    // #######
+    private function getRelativPageNo(): int
+    {
+        return $this->PageNo() - $this->lastResetPageNo;
+    }
+
+    private function getPageMarginNew(int $relativPageNo): array
+    {
+        $marginFirstPage = ['top' => 0, 'bottom' => '45'];
+        $marginDefault = ['top' => 10, 'bottom' => '45'];
+        if ($relativPageNo == 1) {
+            return $marginFirstPage;
+        }
+        return $marginDefault;
+    }
+
+    private function getPageContentHeight(int $relativPageNo): float
+    {
+        $pageHeight = 297; // Unit: mm
+        $pageMargin = $this->getPageMarginNew($relativPageNo);
+        return $pageHeight - $pageMargin['top'] - $pageMargin['bottom'];
+    }
+
+    private function mapY(float $globalY, float $height): array
+    {
+        $globalYCutBefor = 0;
+        $globalYCut = 0;
+        $relativPageNo = 0;
+
+        while (1) {
+            $relativPageNo++;
+            $globalYCutBefor = $globalYCut;
+            $globalYCut += $this->getPageContentHeight($relativPageNo);
+            if ($globalY + $height > $globalYCut) {
+                continue;
+            }
+            $relativY = $globalY - $globalYCutBefor;
+            break;
+        }
+
+        return [
+            'y' => $relativY,
+            'relativPageNo' => $relativPageNo
+        ];
+    }
+
+    private function mapYOnPage(float $globalY, float $height): array
+    {
+        $y = $this->mapY($globalY, $height);
+        $pageMargin = $this->getPageMarginNew($y['relativPageNo']);
+        $y['yOnPage'] = $y['y'] + $pageMargin['top'];
+        return $y;
+    }
+
+    private function doPageBreak(array $y): void
+    {
+        $relativPageNo = $this->getRelativPageNo();
+        if ($relativPageNo < $y['relativPageNo']) {
+            $this->addPage();
+            $this->SetXY(0, 0);
+        }
     }
 }
