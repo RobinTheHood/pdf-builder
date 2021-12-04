d29bc0fac8818db27897a23522fb84afd39c5b4e
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-04 23:38:33 +0100

    chore: improve class PageLineSplitter

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Pdf/PageLineSplitter.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Pdf/PageLineSplitter.php
@@ -12,7 +12,51 @@ class PageLineSplitter
 
     private $pdf = null;
 
-    private function cutLine(float $x1, float $y1, float $x2, float $y2, float $pageSize): array
+
+    // TODO: Calculate X1 and X2, for now only vertical and horizontal are possible
+    public function cutLineNew(float $x1, float $y1, float $x2, float $y2, PageMapper $pageMapper): array
+    {
+        $mappedY1 = $pageMapper->mapY($y1);
+        $mappedY2 = $pageMapper->mapY($y2);
+        $pageStart = $mappedY1['relativPageNo'];
+        $pageEnd = $mappedY2['relativPageNo'];
+        $pages = $pageEnd - $pageStart + 1;
+
+        $yy1 = $mappedY1['y'];
+        if ($y1 == $y2) {
+            return [[
+                'x1' => $x1,
+                'y1' => $mappedY1['y'],
+                'x2' => $x2,
+                'y2' => $mappedY2['y'],
+                'page' => $mappedY1['relativPageNo'],
+            ]];
+        }
+
+        for ($deltaPage = 0; $deltaPage < $pages; $deltaPage++) {
+            $page = $pageStart + $deltaPage;
+            if ($deltaPage < $pages - 1) {
+                $cutLineY = $pageMapper->getPageContentHeight($page);
+            } else {
+                $cutLineY = $mappedY2['y'];
+            }
+            $yy2 = $cutLineY;
+
+            $lines[] = [
+                'x1' => $x1,
+                'y1' => $yy1,
+                'x2' => $x1,
+                'y2' => $yy2,
+                'page' => $page,
+            ];
+
+            $yy1 = 0;
+        }
+
+        return $lines;
+    }
+
+    public function cutLine(float $x1, float $y1, float $x2, float $y2, float $pageSize): array
     {
         $lines = [];
 
