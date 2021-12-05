3f534347afda96b707cd0f283b75606f0e406e93
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-05 11:53:09 +0100

    chore: add missing methods to ContainerRendererCanvasInterface

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/Interfaces/ContainerRendererCanvasInterface.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/Interfaces/ContainerRendererCanvasInterface.php
@@ -8,4 +8,12 @@ interface ContainerRendererCanvasInterface
 {
     public function setColor(float $r, float $g, float $b, float $alpha = 1): void;
     public function drawLine(float $x1, float $y1, float $x2, float $y2): void;
+
+    // Cant rename setFontSizeToDo() -> setFontSize() in Pdf
+    public function setFontSizeToDo(float $size): void;
+
+    // Cant rename setFontToDo() -> setFont() in Pdf
+    public function setFontToDo(string $fontFamily, string $fontStyle, float $fontSize): void;
+
+    public function drawText(string $text, float $x, float $y, float $width, float $height): void;
 }
