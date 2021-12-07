395fd7f9456dea67191f033e2e6b57d8f1ec727c
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-07 17:12:25 +0100

    chore: add drawBorder() (top) to ContainerRenderer

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/ContainerRenderer.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Container/ContainerRenderer.php
@@ -12,31 +12,36 @@ class ContainerRenderer implements ContainerRendererInterface
 {
     public function render(ContainerRendererCanvasInterface $canvas, ContainerInterface $container): void
     {
+        $this->drawBoxes($canvas, $container);
+        $this->drawBorder($canvas, $container);
+        $this->renderChilds($canvas, $container);
+    }
+
+    private function drawBoxes(ContainerRendererCanvasInterface $canvas, ContainerInterface $container)
+    {
         $renderContainer = $container->getCalcedContainer();
 
-        // // Draw Margin Box
-        // $box = $renderContainer->containerBox->getMarginBox();
-        // $color = $this->createColor(255, 0, 0);
-        // $this->drawBox($canvas, $box, $color);
+        // Draw Margin Box
+        $box = $renderContainer->containerBox->getMarginBox();
+        $color = $this->createColor(255, 0, 0);
+        $this->drawBox($canvas, $box, $color);
 
-        // if (true) {
-        //     // Draw Border Box
-        //     $box = $renderContainer->containerBox->getBorderBox();
-        //     $color = $this->createColor(0, 255, 0);
-        //     $this->drawBox($canvas, $box, $color);
+        if (true) {
+            // Draw Border Box
+            $box = $renderContainer->containerBox->getBorderBox();
+            $color = $this->createColor(0, 255, 0);
+            $this->drawBox($canvas, $box, $color);
 
-        //     // Draw Padding Box
-        //     $box = $renderContainer->containerBox->getPaddingBox();
-        //     $color = $this->createColor(0, 0, 255);
-        //     $this->drawBox($canvas, $box, $color);
+            // Draw Padding Box
+            $box = $renderContainer->containerBox->getPaddingBox();
+            $color = $this->createColor(0, 0, 255);
+            $this->drawBox($canvas, $box, $color);
 
-        //     // Draw Content Box
-        //     $box = $renderContainer->containerBox->getContentBox();
-        //     $colorBlue = $this->createColor(0, 255, 255);
-        //     $this->drawBox($canvas, $box, $colorBlue);
-        // }
-
-        $this->renderChilds($canvas, $container);
+            // Draw Content Box
+            $box = $renderContainer->containerBox->getContentBox();
+            $colorBlue = $this->createColor(0, 255, 255);
+            $this->drawBox($canvas, $box, $colorBlue);
+        }
     }
 
     private function renderChilds(ContainerRendererCanvasInterface $canvas, ContainerInterface $container)
@@ -44,6 +49,22 @@ class ContainerRenderer implements ContainerRendererInterface
         foreach ($container->getChildContainers() as $childContainer) {
             $containerRenderer = $childContainer->getContainerRenderer();
             $containerRenderer->render($canvas, $childContainer);
+        }
+    }
+
+    private function drawBorder(ContainerRendererCanvasInterface $canvas, ContainerInterface $container)
+    {
+        $renderContainer = $container->getCalcedContainer();
+        $borderBox = $renderContainer->containerBox->getBorderBox();
+
+        $color = $renderContainer->containerBox->borderColor;
+        $canvas->setColor($color['r'], $color['g'], $color['b']);
+
+        // Top
+        if ($renderContainer->containerBox->borderTop->isSet()) {
+            $boxLinesTop = $borderBox['boxLines']['top'];
+            $canvas->setLineWidthToDo($renderContainer->containerBox->borderTop->getValue());
+            $canvas->drawLine($boxLinesTop['x1'], $boxLinesTop['y1'], $boxLinesTop['x2'], $boxLinesTop['y2']);
         }
     }
 
