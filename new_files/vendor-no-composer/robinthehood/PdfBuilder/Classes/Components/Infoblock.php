0c46b1da40eee41b69d1ab38b1de67bfe56fd895
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-07 12:16:53 +0100

    chore: improve demo Infoblock

--- new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Components/Infoblock.php
+++ new_files/vendor-no-composer/robinthehood/PdfBuilder/Classes/Components/Infoblock.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace RobinTheHood\PdfBuilder\Classes\Components;
 
 use RobinTheHood\PdfBuilder\Classes\Container\Container;
+use RobinTheHood\PdfBuilder\Classes\Elements\Table;
+use RobinTheHood\PdfBuilder\Classes\Pdf\Pdf;
 
 class Infoblock extends Container
 {
@@ -22,5 +24,41 @@ class Infoblock extends Container
         $this->containerBox->positionY->setValue($this->basePositionY);
         $this->containerBox->width->setValue($this->baseWidth);
         $this->containerBox->height->setValue($this->baseHeight);
+
+        $table = new Table();
+
+        $table->setColumnWidths([
+            30,
+            20
+        ]);
+
+        $rowSettings = [
+            'fontWeight' => Pdf::FONT_STYLE_NORMAL,
+            'border' => Table::ROW_BORDER_BOTTOM,
+            'fontSize' => 10,
+            'paddingBottom' => 2
+        ];
+
+        $table->addRow([
+            ['content' => 'Rechnung'], ['content' => 'R-123456']
+        ], $rowSettings);
+
+        $table->addRow([
+            ['content' => 'Datum'], ['content' => '24.12.2021']
+        ], $rowSettings);
+
+        $table->addRow([
+            ['content' => 'Kunde'], ['content' => 'K-123456']
+        ], $rowSettings);
+
+        $table->addRow([
+            ['content' => 'Bestellung'], ['content' => 'O-123456']
+        ], $rowSettings);
+
+        $table->addRow([
+            ['content' => 'Zahnungsart'], ['content' => 'Vorkasse']
+        ], $rowSettings);
+
+        $this->addChildContainer($table);
     }
 }
